9bc4c9c36e127ea6832d46d520e1036efc425a39
xiangdd <xiangdd@yoozoo.com>
2020-05-27 21:31:58 +0800

    1.代码压缩

--- fun/VersionManage.go
+++ fun/VersionManage.go
@@ -10,6 +10,7 @@ import (
 	"nodeAgent/tools"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"regexp"
 	"strings"
 )
@@ -94,8 +95,23 @@ func ParseChange(path string, result string) []string {
 	return res
 }
 
-func PackageFiles(files []string) {
-
+func PackageFiles(path, source, desc string) error {
+	//判断路径是否存在
+	if !tools.IsExist(source) {
+		log.Fatal("program is not Exist")
+	}
+	//打开目录
+	cmd := exec.Command("tar", "-zcvf", desc, source)
+	cmd.Dir = path
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	res := out.String()
+	fmt.Println(res)
+	return nil
 }
 
 //压缩 使用gzip压缩成tar.gz
@@ -107,14 +123,98 @@ func Compress(files []*os.File, dest string) error {
 	tw := tar.NewWriter(gw)
 	defer tw.Close()
 	for _, file := range files {
-		err := compress(file, "", tw)
+		err := compress(file, "test", tw)
 		if err != nil {
 			return err
 		}
 	}
 	return nil
 }
+func CreateTar(fileSource, fileTarget string) error {
+	//创建目录
+	tarTarget, err := os.Create(fileTarget)
+	if err != nil {
+		//如果文件存在删除文件
+		if err == os.ErrExist {
+			if err := os.Remove(fileTarget); err != nil {
+				fmt.Println(err)
+				return err
+			}
+		} else {
+			fmt.Println(err)
+			return err
+		}
+	}
+	defer tarTarget.Close()
+	tarWriter := tar.NewWriter(tarTarget)
+	sFileInfo, err := os.Stat(fileSource)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	if !sFileInfo.IsDir() {
+		return tarFile(fileTarget, fileSource, sFileInfo, tarWriter)
+	} else {
+		return tarFolder(fileSource, tarWriter)
+	}
+	return nil
+}
 
+func tarFile(directory string, filesource string, sfileInfo os.FileInfo, tarwriter *tar.Writer) error {
+	sfile, err := os.Open(filesource)
+	if err != nil {
+		panic(err)
+		return err
+	}
+	defer sfile.Close()
+	header, err := tar.FileInfoHeader(sfileInfo, "")
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	header.Name = directory
+	err = tarwriter.WriteHeader(header)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	if _, err = io.Copy(tarwriter, sfile); err != nil {
+		fmt.Println(err)
+		panic(err)
+		return err
+	}
+	return nil
+}
+
+func tarFolder(directory string, tarwriter *tar.Writer) error {
+	var baseFolder string = filepath.Base(directory)
+	//fmt.Println(baseFolder)
+	return filepath.Walk(directory, func(targetpath string, file os.FileInfo, err error) error {
+		//read the file failure
+		if file == nil {
+			panic(err)
+			return err
+		}
+		if file.IsDir() {
+			// information of file or folder
+			header, err := tar.FileInfoHeader(file, "")
+			if err != nil {
+				return err
+			}
+			header.Name = filepath.Join(baseFolder, strings.TrimPrefix(targetpath, directory))
+			fmt.Println(header.Name)
+			if err = tarwriter.WriteHeader(header); err != nil {
+				return err
+			}
+			os.Mkdir(strings.TrimPrefix(baseFolder, file.Name()), os.ModeDir)
+			return nil
+		} else {
+			//baseFolder is the tar file path
+			var fileFolder = filepath.Join(baseFolder, strings.TrimPrefix(targetpath, directory))
+			return tarFile(fileFolder, targetpath, file, tarwriter)
+		}
+	})
+}
 func compress(file *os.File, prefix string, tw *tar.Writer) error {
 	info, err := file.Stat()
 	if err != nil {
@@ -123,6 +223,7 @@ func compress(file *os.File, prefix string, tw *tar.Writer) error {
 	if info.IsDir() {
 		prefix = prefix + "/" + info.Name()
 		fileInfos, err := file.Readdir(-1)
+		fmt.Println(fileInfos)
 		if err != nil {
 			return err
 		}
@@ -186,6 +287,7 @@ func DeCompress(tarFile, dest string) error {
 	}
 	return nil
 }
+
 func GetFilesByName(path string, files []string) []*os.File {
 	var filesOs []*os.File
 	for _, filename := range files {
@@ -199,9 +301,49 @@ func GetFilesByName(path string, files []string) []*os.File {
 	}
 	return filesOs
 }
+
+//移动文件
+func MoveFiles(path string, files []string, des string) {
+	for _, filename := range files {
+		//fmt.Println(path + filename)
+		//判断路径是否存在并创建
+		err := os.MkdirAll(string([]rune(path + des + filename)[0:strings.LastIndex(path+des+filename, "/")]), 0755)
+		if err != nil {
+			log.Println(err)
+		}
+		_, err = copyFile(path+filename, path+des+filename)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+//文件拷贝
+func copyFile(src, dst string) (int64, error) {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return 0, err
+	}
+	if !sourceFileStat.Mode().IsRegular() {
+		return 0, fmt.Errorf("%s is not a regular file", src)
+	}
+	source, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	defer source.Close()
+	destination, err := os.Create(dst)
+	if err != nil {
+		return 0, err
+	}
+	defer destination.Close()
+	nBytes, err := io.Copy(destination, source)
+	return nBytes, err
+}
+
 func createFile(name string) (*os.File, error) {
 	fmt.Println(name)
-	err := os.MkdirAll(string([]rune(name)[0:strings.LastIndex(name, "\\")]), 0755)
+	err := os.MkdirAll(string([]rune(name)[0:strings.LastIndex(name, "/")]), 0755)
 	if err != nil {
 		return nil, err
 	}
--- test/version_test.go
+++ test/version_test.go
@@ -19,6 +19,9 @@ func TestCompareVersion(t *testing.T) {
 
 func TestPackageFiles(t *testing.T) {
 	files := fun.CompareCode("/Users/xiangdd/go/src/goAgent/", "05cc39097af99b00c771205077fe73514c32c322", "464d8e8e24e83397737f3cee4562212f9d13fbae")
-	filesOs := fun.GetFilesByName("/Users/xiangdd/go/src/goAgent/", files)
-	fun.Compress(filesOs, "code/version/test.tar.gz")
+	fun.MoveFiles("/Users/xiangdd/go/src/goAgent/", files, "code_version/tmp/")
+	fun.PackageFiles("/Users/xiangdd/go/src/goAgent/code_version/", "tmp", "test.tar.gz")
+	//filesOs := fun.GetFilesByName("/Users/xiangdd/go/src/goAgent/code_version/tmp/",files)
+	//err:=fun.Tar2("/Users/xiangdd/go/src/goAgent/code_version/tmp", "/Users/xiangdd/go/src/goAgent/code_version/test.tar",false)
+	//fun.CreateTar("../code_version/tmp", "/Users/xiangdd/go/src/goAgent/code_version/test.tar.gz")
 }
